079b2ff63eb8bd5edda3345b2d3c66de6fd962e9
Jeus <alkhandani+sava@gmail.com>
2020-02-26 01:47:49 +0300

    configuration code

---
+++ src/main/java/com/hosp/occupancy/rest/SampleController.java
@@ -0,0 +1,17 @@
+package com.hosp.occupancy.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class SampleController {
+
+    @GetMapping
+    public String sayHello() {
+        return "hello world";
+    }
+
+
+}
